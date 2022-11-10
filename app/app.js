82d825a96c5a78f62a7749471156b49d963fb9ae
devcloudrp <rakesh.pillai@sreyo.com>
2022-11-10 12:25:48 -0500

    updated app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send('We are on home');
+    res.send('We are away from home');
 });
 
 app.listen(3000);
